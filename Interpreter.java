e5fabb13be7a30fce0ca458536231f86d3d57d5c
Isaque Bezerra Vieira <bezerra.isaque@gmail.com>
2017-02-04 16:57:27 -0400

    Definicao de variavel

--- Interpreter.java
+++ Interpreter.java
@@ -1,6 +1,8 @@
 import java.lang.Exception;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 enum Operator { ADD, SUB, MUL, DIV, RST }
 enum OpRel { GTR, LSS, EQL, GEQL, LEQL, DIF }
@@ -41,15 +43,42 @@ class IfExp extends ASTExp {
 class ParamExp extends ASTExp {
     List<ASTExp> list;
 
-    ParamExp(ASTExp e) {
+    ParamExp() {
         list = new ArrayList<ASTExp>();
-        list.add(e);
     }
 }
 
 class Ident extends ASTExp {
     String name;
-    Ident(String n) {
+    Ident(String n){
+        name = n;
+    }
+}
+
+class DefVar extends ASTExp {
+    String name;
+    DefVar(String n) {
+        name = n;
+    }
+}
+
+class Var extends ASTExp {
+    String name;
+    Var(String n) {
+        name = n;
+    }
+}
+
+class DefFunc extends ASTExp {
+    String name;
+    DefFunc(String n) {
+        name = n;
+    }
+}
+
+class Func extends ASTExp {
+    String name;
+    Func(String n) {
         name = n;
     }
 }
@@ -78,61 +107,84 @@ class BoolConst extends ASTExp {
 
 class Interpreter
 {
+    Map<String, ASTExp> definicoes = new HashMap<String, ASTExp>();
+
     public ASTExp eval(ASTExp exp) throws Exception
     {
         if(exp == null){
-            System.out.println("ast == null");
-        }
-        else if (exp instanceof NumberConst){
-            return exp;
-        }
-        else if(exp instanceof UnaryExp){
-            ASTExp e = eval(exp.left);
-            if (e instanceof NumberConst) {
-                switch(((UnaryExp) exp).op) {
-                    case SUB: return new NumberConst(0 - ((NumberConst) e).val);
-                }
-            }
-        }
-        else if (exp instanceof IfExp) {
-            BoolConst e = (BoolConst) eval(exp.left);
-            if (e.val) {
-                return eval(((IfExp) exp).middle);
-            }
-            else {
-                return eval(exp.right);
-            }
-        }
-        else if (exp instanceof ExpL) {
-            ASTExp v1 = eval(exp.left);
-            ASTExp v2 = eval(exp.right);
-            if(v1 instanceof NumberConst && v2 instanceof NumberConst){
-                NumberConst b1, b2;
-                b1 = (NumberConst) v1;
-                b2 = (NumberConst) v2;
-                switch(((ExpL) exp).op) {
-                    case GTR: return new BoolConst(b1.val > b2.val);
-                    case LSS: return new BoolConst(b1.val < b2.val);
-                    case EQL: return new BoolConst(b1.val == b2.val);
-                    case GEQL: return new BoolConst(b1.val >= b2.val);
-                    case LEQL: return new BoolConst(b1.val <= b2.val);
-                    case DIF: return new BoolConst(b1.val != b2.val);
-                }
-            }
+            throw new Error("Erro na AST");
         }
         else {
-            ASTExp v1 = eval(exp.left);
-            ASTExp v2 = eval(exp.right);
-            if(v1 instanceof NumberConst && v2 instanceof NumberConst){
-                NumberConst n1, n2;
-                n1 = (NumberConst) v1;
-                n2 = (NumberConst) v2;
-                switch(((BinExp) exp).op) {
-                    case ADD: return new NumberConst(n1.val + n2.val);
-                    case SUB: return new NumberConst(n1.val - n2.val);
-                    case MUL: return new NumberConst(n1.val * n2.val);
-                    case DIV: return new NumberConst(n1.val / n2.val);
-                    case RST: return new NumberConst(n1.val % n2.val);
+            if (exp instanceof DefVar) {
+                DefVar var = (DefVar) exp;
+                ASTExp e = eval(var.left);
+                definicoes.put(var.name, e);
+                return e;
+            }
+            else if (exp instanceof Var) {
+                return eval(exp.left);
+            }
+            else if (exp instanceof Ident){
+                String name = ((Ident) exp).name;
+                ASTExp e = definicoes.get(name);
+                if (e == null) {
+                    throw new Error(name + " not defined");
+                }
+                else {
+                    return eval(e);
+                }
+            }
+            else if (exp instanceof NumberConst){
+                return exp;
+            }
+            else if(exp instanceof UnaryExp){
+                ASTExp e = eval(exp.left);
+                if (e instanceof NumberConst) {
+                    switch(((UnaryExp) exp).op) {
+                        case SUB: return new NumberConst(0 - ((NumberConst) e).val);
+                    }
+                }
+            }
+            else if (exp instanceof IfExp) {
+                BoolConst e = (BoolConst) eval(exp.left);
+                if (e.val) {
+                    return eval(((IfExp) exp).middle);
+                }
+                else {
+                    return eval(exp.right);
+                }
+            }
+            else if (exp instanceof ExpL) {
+                ASTExp v1 = eval(exp.left);
+                ASTExp v2 = eval(exp.right);
+                if(v1 instanceof NumberConst && v2 instanceof NumberConst){
+                    NumberConst b1, b2;
+                    b1 = (NumberConst) v1;
+                    b2 = (NumberConst) v2;
+                    switch(((ExpL) exp).op) {
+                        case GTR: return new BoolConst(b1.val > b2.val);
+                        case LSS: return new BoolConst(b1.val < b2.val);
+                        case EQL: return new BoolConst(b1.val == b2.val);
+                        case GEQL: return new BoolConst(b1.val >= b2.val);
+                        case LEQL: return new BoolConst(b1.val <= b2.val);
+                        case DIF: return new BoolConst(b1.val != b2.val);
+                    }
+                }
+            }
+            else {
+                ASTExp v1 = eval(exp.left);
+                ASTExp v2 = eval(exp.right);
+                if(v1 instanceof NumberConst && v2 instanceof NumberConst){
+                    NumberConst n1, n2;
+                    n1 = (NumberConst) v1;
+                    n2 = (NumberConst) v2;
+                    switch(((BinExp) exp).op) {
+                        case ADD: return new NumberConst(n1.val + n2.val);
+                        case SUB: return new NumberConst(n1.val - n2.val);
+                        case MUL: return new NumberConst(n1.val * n2.val);
+                        case DIV: return new NumberConst(n1.val / n2.val);
+                        case RST: return new NumberConst(n1.val % n2.val);
+                    }
                 }
             }
         }
